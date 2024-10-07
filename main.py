2da4f0fd0dea3e24b462247bc697991d54d47bd5
deloygintaras <turcinasgintaras@gmail.com>
2024-10-07 21:25:39 +0300

    updted

--- main.py
+++ main.py
@@ -76,6 +76,7 @@
 # import random
 # from audioop import reverse
 # from re import search
+# from audioop import reverse
 from idlelib.replace import replace
 
 # 2. Susikurkite list žmonių ūgiams saugoti ir užpildykite jį informacija.
@@ -314,7 +315,7 @@ from idlelib.replace import replace
 # ]
 # indeksas = 0
 # while indeksas < len(sarasas):
-#     print(f"Mes jau mokemes:",indeksas, sarasas[indeksas])
+#     print(f"Mes jau mokemes:",indeksas + 1, sarasas[indeksas])
 #     indeksas += 1
 
 
@@ -336,25 +337,223 @@ from idlelib.replace import replace
 # indeksas = 0
 # print("Pasirinktinos studiju programos:")
 # while indeksas < len(studijos):
-#     print(f"Studiju programa:", indeksas, studijos[indeksas])
+#     print(f"Studiju programa:", indeksas + 1, studijos[indeksas])
 #     indeksas += 1
 
 
 # 21.Susikurkite masyvą šalių pavadinimams saugoti ir jį užpildykite
 # duomenimis. Išveskite šalių pavadinimus atskirose eilutėse, su prierašu
 # "šalis" ir tada nurodant šalį iš masyvo.
-
-
-
+#
+# salis = [
+#     "Anglija",
+#     "Svedija",
+#     "Amerika",
+#     "Vokietija",
+#     "Danija",
+#     "Estija",
+#     "Lenkija",
+#     "Australija",
+#     "Prancuzija",
+#     "Ispanija",
+#     "Portugalija"
+# ]
+# indeksas = 0
+# while indeksas < len(salis):
+#     print(f"Salis :", indeksas + 1, salis[indeksas])
+#     indeksas += 1
 
 
 # 22.Susikurkite sąrašą prekių krepšeliui saugoti. Išveskite kiek prekių
 # krepšelyje yra narių. Tuomet išveskite visą prekių krepšelio informaciją,
 # nurodant kelinta prekė, pvz "nr 1 pirkinys:", "nr 2 pirkinys:" ir t.t.
 
+# prekes = ["obuolys", "kriause", "agurkas", "pomidoras", "abrikosas", "mandarinas", "morka", "cesnakas"]
+# print("Prekiu krepselyje yra:", len(prekes), "prekiu.")
+# indeksas = 0
+# while indeksas < len(prekes):
+#     print("Nr:", indeksas + 1,"pirkinys -", prekes[indeksas])
+#     indeksas += 1
 
 
 # 23.Susikurkite pažymių sąrašą ir užpildykite jį informacija. Surikiuokite
 # pažymius nuo didžiausio iki mažiausio. Išveskite visus turimus pažymius
 # atskirose eilutėse. Prie kiekvieno pažymio taip pat prirašykite "puikiai",
 # jeigu jis yra 10, "labai gerai", jeigu jis yra 9 ir t.t.
+
+# pazpav = ["labai blogai", "blogai", "dar vis blogai", "puse velnio", "sriednas", "neblogai", "pusiau gerai", "gerai", "labai gerai", "puikiai"]
+# pazymiai = [4, 5, 6, 1, 2, 3, 7, 8, 9, 10]
+# sortiruote = (sorted(pazymiai, reverse = True))
+# print(sortiruote)
+# indeksas = 0
+# while indeksas < len(sortiruote):
+#     print(f"Pazymys", indeksas + 1, '-', pazpav[indeksas])
+#     indeksas += 1
+
+
+# 24.Susikurkite programą, kurioje vartotojas galėtų nusakyti kiek atsitiktinių
+# skaičių turėtų būti sugeneruota. Tuomet programa turėtų būtent tokį kiekį
+# atsitiktinių skaičių sugeneruoti ir sudėti į sąrašą. Išveskite visus šiuos
+# skaičius ekrane. Tuomet tuos pačius skaičius išveskite ekrane dar kartą,
+# tačiau viską spausdinkite atskirose eilutėse, eilutėje nurodant patį skaičių
+# ir jo kvadratą.
+
+
+
+
+
+# 25.Susikurkite norimą sąrašą su duomenimis. Išsiveskite viso šio sąrašo
+# informaciją. Pakeiskite trijų pasirinktų narių reikšmes į kitas. Išsiveskite
+# pakeisto sąrašo informaciją.
+
+
+
+
+
+
+# 26.Susikurkite sąrašą ir jį užpildykite skaičiais (savarankiškai arba
+# atsitiktiniais). Iš pradžių išveskite tekstą "lyginiai skaičiai" ir visus lyginius
+# skaičius. Tuomet išveskite tekstą "visi nelyginiai skaičiai" ir visus nelyginius
+# skaičius. Bei ant galo tekstą "visi skaičiai, kurie dalinasi iš 3" ir visus
+# skaičius, kurie atitinka tokią sąlygą.
+
+
+
+
+# 27.Susikurkite sąrašą ir jį užpildykite atsitiktiniais skaičiais. Išveskite visus
+# skaičius didesnius nei vidurkis.
+
+
+
+
+
+# 28.Susikurkite programą, kurioje būtų sukurtas sąrašas iš pasirinkto kiekio
+# atsitiktinių skaičių. Raskite kiekvieno skaičiaus daliklius, pavyzdžiui:
+# skaičius 2 dalinasi iš 2
+# skaičius 3 dalinasi iš 3
+# skaičius 4 dalinasi iš 2, 4
+# skaičius 5 dalinasi iš 5
+# skaičius 6 dalinasi iš 2, 3, 6
+
+
+
+
+# 29.Sukurkite programą, kurioje vartotojas galėtų įvesti norimą kiekį žodžių
+# (pasirenka iš pradžių ir vykdomas for iki to kiekio skaičiaus, arba
+# vykdomas while kol neįveda q ar kokio kito simbolio/žodžio). Išveskite
+# visus šiuos žodžius ekrane.
+
+
+
+# 30.Susikurkite sąrašą iš pasirinktų žodžių. Atskirose eilutėse išveskite patį
+# žodį, jo raidžių kiekį.
+
+
+
+
+# 31.Susikurkite pažymių sąrašą. Šiuos pažymius leiskite įvesti vartotojui.
+# Baigus įvedimą, išveskite suvestų pažymių vidurkį. Taip pat, jeigu
+# studentas turėjo neigiamų pažymių, tuomet atskirai parodykite tuos
+# neigiamus pažymius ir jų kiekį.
+
+
+
+# 32.Susikurkite žodžių sąrašą. Raskite ir išveskite trumpiausią ir ilgiausią
+# žodžius, bei jų raidžių kiekius.
+
+
+
+
+# 33.Sugeneruokite 100-ą atsitiktinių skaičių ir sukelkite visus tuos skaičius į
+# sąrašą. Atlikite šiuos veiksmus:
+# a. Raskite mažiausią skaičių, didžiausią skaičių, bei vidurkį.
+# b. Tuomet atrinkite į atskirą sąrašą skaičius, kurie žemesni nei vidurkis. Raskite šių skaičių
+# vidurkį.
+# c. Taip pat, atrinkite į dar vieną sąrašą skaičius, kurie didesni nei vidurkis. Raskite šių skaičių
+# vidurkį.
+# d. Ekrane išveskite pradinius duomenis, mažiausią ir didžiausią skaičius, rastą vidurkį,
+# pirmus atrinktus skaičius ir jų vidurkį, antrus atrinktus skaičius ir jų vidurkį.
+
+
+
+
+# 34.Susikurkite žodžių masyvą. Išveskite per kiek simbolių yra ilgesnis
+# ilgiausias žodis už trumpiausią. Tarkime ilgiausias yra “kompiuteris” (11
+# simbolių), o trumpiausias “medis” (5), skirtumas tarp jų 11 - 5 = 6
+# simboliai.
+
+
+
+
+# 35.Susikurkite dviejų studentų pažymių sąrašus. Šią informaciją leiskite
+# suvesti vartotojui. Raskite kurio studento vidurkis yra geresnis. Taip pat,
+# ar kuris iš jų (ar abu) turi neigiamų pažymių.
+
+
+# 36.Susikurkite skaičių masyvą ir užpildykite jį atsitiktiniais skaičiais. Raskite
+# visų skaičių, kurie dalinasi iš 4 sumą.
+
+
+
+# 37.Susikurkite skaičių masyvą ir užpildykite jį duomenimis. Išveskite visus
+# skaičius atskirose eilutėse. Prie kiekvieno lyginio skaičiaus dar išvedant jo
+# kvadratą.
+
+
+
+# 38.Susikurkite studento pažymių sąrašą ir užpildykite jį duomenimis
+# (atsitiktiniais arba kokius įrašysite). Išveskite kiekvieną pažymį atskiroje
+# eilutėje. Prie kiekvieno pažymio nurodykite ar tai teigiamas, ar neigiamas
+# pažymys. Taip pat, jeigu neigiamas pažymys, tuomet dar nurodykite kiek
+# balų trūko iki teigiamo pažymio. Teigiamas pažymys skaitosi 5 balai.
+
+
+
+
+# 39.Susikurkite žodžių sąrašą ir užpildykite duomenimis. Išveskite visus
+# žodžius ekrane, nurodant iš kiek raidžių kiekvienas šis žodis yra sudarytas.
+# Papildykite programą taip, kad rastumėte visų raidžių kiekį (pvz yra žodžiai
+# "medis" ir "alus", 5 ir 4 raidės atitinkamai, o jas sudėjus gaunasi 9 raidės).
+
+
+
+
+# 40.Susikurkite skaičių sąrašą ir užpildykite duomenimis. Raskite visų skaičių,
+# kurie dalinasi iš 3 sumą ir vidurkį. Išveskite pradinius duomenis ir gautus
+# atsakymus.
+
+
+
+
+# 41.Susikurkite sąrašą failų pavadinimams saugoti, užpildykite jį duomenimis.
+# Įsivaizduokite kad jums reikės nuskaityti šiuos failus, todėl pirma norėsite
+# patikrinti su kuriais galite dirbti. Atrinkite į atskirą sąrašą tik tuos failus,
+# kurių galūnė yra .txt arba .json tipo. Išveskite atrinktus duomenis.
+
+
+
+
+# 42.Susikurkite sąrašą įvykusių klaidų kodams saugoti ir užpildykite šį masyvą
+# duomenimis. Tuomet išveskite visas sukauptas klaidas administratoriui,
+# taip, kad jis suprastų kas per klaidos įvyko. Pavyzdžiui, jeigu yra kodas
+# "ui87", tai kad išvestų "Grafinės sąsajos klaida navigacijoje", arba jeigu
+# kodas "sys12", tuomet "Trūksta operatyviosios atminties sistemoje" ir
+# pan.
+
+
+
+
+
+# 43.Susikurkite sąrašą sandėlio likučiams saugoti (kiekvienas atskiras narys
+# sąraše yra atskiros prekės likutis). Su kiekvienu likučiu paskaičiuokite per
+# kiek dienų bus išpirktas, jei per dieną vidutiniškai yra nuperkami 5 vnt.
+# Išveskite atsakymus atskirose eilutėse, nurodant kiek yra dabar ir kiek
+# dienų užteks jo. Pavyzdžiui, jeigu yra likučiai 74, 54 ir 32, tai 74 vnt. prekės
+# užteks maždaug 15 dienų, 54 vnt. prekės užteks maždaug 11 dienų ir t.t.
+# Pabandykite papildyti programą taip, kad į atskirą sąrašą atrinktų tas
+# prekes, kurių užteks savaitei ar mažiau, jas išveskite atskirai, pačioje
+# pabaigoje.
+
+# 44.Susikurkite sąrašą norimiems žodžiams saugoti. Užpildykite šį sąrašą
+# duomenimis. Į kitą sąrašą atrinkite tuos žodžius, kurie yra trumpi (sudaro
+# mažiau nei 5 raidės). Išveskite pradinius duomenis ir atrinktus.
